c7f26ddd1bb4f996832da6d326e564528dd5b2f2
YeeunYou <emilyyou95@gmail.com>
2016-02-12 17:57:55 -0500

    newTask functionality working/ tested with index.html

--- process.php
+++ process.php
@@ -1,18 +1,28 @@
 <?php
-header("Content-Type: application/json");
+header("Content-Type: application/json"); 
 if(isset($_GET['action']))
-{
+{ 
 	require_once('connect.php');
 	$conn->select_db("lamp2proj1");
 
-	if($_GET['action'] == 'list' && $_SERVER['REQUEST_METHOD'] == 'GET')
+	if($_SERVER['REQUEST_METHOD'] == 'GET')
 	{
-		listTasks($conn);
+		if($_GET['action'] == 'list')
+		{
+			listTasks($conn);
+		}
+		else if($_GET['sortby'] == 'priority')
+		{
+			byPriority($conn);
+		}
 	}
-	else if($_GET['action'] == 'new' && $_SERVER['REQUEST_METHOD'] == 'POST')
+	else if($_SERVER['REQUEST_METHOD'] == 'POS')
 	{
-		newTask($conn);
-	}
+		if($_GET['action'] == 'new')
+		{
+			newTask($conn);
+		}
+	} 
 	mysqli_close($conn);
 }
 
@@ -38,21 +48,19 @@ function newTask($conn)
 	
 	$insert = $conn->query("INSERT INTO task(description, priority, dateCreated, completed, dateCompleted) VALUES ('" . $description ."','" . $priority . "' , NOW(), 0, '0000-00-00 00:00:00')");
 	$conn->query($insert);
-	if($conn->affected_rows > 0)
-	{
-		$countQry = sprintf("SELECT count(id) AS count FROM task");
-		$countRS = mysqli_query($conn, $countQry);
-		$lastID = $countRS->fetch_object()->count;
 	
-		$query = "SELECT id, description, priority, dateCreated, dateCompleted FROM task WHERE id = $lastID ORDER BY dateCreated ASC";
-		if($result = $conn->query($query))
+	$countQry = sprintf("SELECT count(id) AS count FROM task");
+	$countRS = mysqli_query($conn, $countQry);
+	$lastID = $countRS->fetch_object()->count;
+
+	$query = "SELECT id, description, priority, dateCreated, dateCompleted FROM task WHERE id = $lastID ORDER BY dateCreated ASC";
+	if($result = $conn->query($query))
+	{
+		while($task = mysqli_fetch_assoc($result))
 		{
-			while($task = mysqli_fetch_assoc($result))
-			{
-				array_push($taskArr, $task);
-			}
-			echo json_encode($taskArr);
+			array_push($taskArr, $task);
 		}
+		echo json_encode($taskArr);
 	}
 }
 ?>
